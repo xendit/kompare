05b6ed0c74fd85884253f3c1568eb63b64429c52
abel.guzman <abel.guzman@MBP-FW2K0J3JFJ.local>
2024-01-19 15:26:41 +0800

    working queries and example main call to some of those queries.

--- query/query.go
+++ query/query.go
@@ -4,17 +4,24 @@ import (
 	"context"
 
 	v1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
+	batchv1 "k8s.io/api/batch/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+
 	Corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-type KCMetadata struct {
-	namespaceName string
+func ListNameSpaces(clientset *kubernetes.Clientset) *Corev1.NamespaceList {
+	nsList, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return nsList
 }
 
 func ListK8sDeployments(clientset *kubernetes.Clientset, nameSpace string) *v1.DeploymentList {
-
 	if nameSpace == "" {
 		nameSpace = "default"
 	}
@@ -25,10 +32,37 @@ func ListK8sDeployments(clientset *kubernetes.Clientset, nameSpace string) *v1.D
 	return deployments_list
 }
 
-func ListNameSpaces(clientset *kubernetes.Clientset) *Corev1.NamespaceList {
-	nsList, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+func ListHPAs(clientset *kubernetes.Clientset, nameSpace string) *autoscalingv1.HorizontalPodAutoscalerList {
+	if nameSpace == "" {
+		nameSpace = "default"
+	}
+	listHPA, err := clientset.AutoscalingV1().HorizontalPodAutoscalers(nameSpace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
-	return nsList
+	return listHPA
+}
+
+func ListCronJobs(clientset *kubernetes.Clientset, nameSpace string) *batchv1.CronJobList {
+	listCronJobs, err := clientset.BatchV1().CronJobs(nameSpace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return listCronJobs
+}
+
+func ListIngressRoutes(clientset *kubernetes.Clientset, nameSpace string) *networkingv1.IngressList {
+	listIngress, err := clientset.NetworkingV1().Ingresses(nameSpace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return listIngress
+}
+
+func ListServices(clientset *kubernetes.Clientset, nameSpace string) *Corev1.ServiceList {
+	listServices, err := clientset.CoreV1().Services(nameSpace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return listServices
 }

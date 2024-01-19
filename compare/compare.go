e558af5bf1092da818c2ca035bc81d6f56870aa5
Abel Guzman Sanchez <abel.guzman@xendit.co>
2024-01-19 17:27:05 +0800

    compare deployments organized and returning data instead of just printing stuff

--- compare/compare.go
+++ compare/compare.go
@@ -7,7 +7,7 @@ import (
 	v1 "k8s.io/api/apps/v1"
 )
 
-// source vs target or old vs new
+// source vs target or old vs new or First vs Second
 func CompareNumberOfDeployments(sourceDeployments, targetDeplotments *v1.DeploymentList) (int, int) {
 	//Print quantity of deployments
 	len_sourceDeployments := len(sourceDeployments.Items)
@@ -18,20 +18,23 @@ func CompareNumberOfDeployments(sourceDeployments, targetDeplotments *v1.Deploym
 	return len_sourceDeployments, len_targetDeplotments
 }
 
-func iterateSimpleDiff(sourceDeployments, targetDeplotments *v1.DeploymentList) {
+func iterateSimpleDiff(sourceDeployments, targetDeplotments *v1.DeploymentList) ([]string, []string) {
 	len_sourceDeployments, len_targetDeplotments := CompareNumberOfDeployments(sourceDeployments, targetDeplotments)
 	if len_sourceDeployments != len_targetDeplotments {
+		var onlyInSource, onlyInTarget []string
+
 		fmt.Printf("NOTICE NOT EQUAL NUMBER OF DEPLOYMENTS!!!\n")
-		compareDeploymentsByName(sourceDeployments, targetDeplotments,
-			"- First cluster has deployment %s, but it's not in the second cluster\n")
-		compareDeploymentsByName(targetDeplotments, sourceDeployments,
-			"- Second cluster has deployment %s, but it's not in the first cluster\n")
-	} else {
-		deepDeployFirstSecondCompare(targetDeplotments, sourceDeployments, show_replicas)
+		onlyInSource = compareDeploymentsByName(sourceDeployments, targetDeplotments,
+			"- Source cluster has deployment %s, but it's not in the second cluster\n")
+		onlyInTarget = compareDeploymentsByName(targetDeplotments, sourceDeployments,
+			"- target cluster has deployment %s, but it's not in the first cluster\n")
+		return onlyInSource, onlyInTarget
 	}
+	return nil, nil
 }
 
-func compareDeploymentsByName(first_deployments, second_deployments *v1.DeploymentList, message_heading string) {
+func compareDeploymentsByName(first_deployments, second_deployments *v1.DeploymentList, message_heading string) []string {
+	var diffNameList []string
 	for _, d := range first_deployments.Items {
 		exists := false
 		for _, b := range second_deployments.Items {
@@ -41,10 +44,13 @@ func compareDeploymentsByName(first_deployments, second_deployments *v1.Deployme
 		}
 		if exists == false {
 			fmt.Printf(strings.Replace(message_heading, "%s", d.Name, -1))
+			diffNameList = append(diffNameList, d.Name)
 		}
 	}
+	return diffNameList
 }
 
+// TODO
 func deepDeployFirstSecondCompare(sourceDeployments, targetDeplotments *v1.DeploymentList, show_replicas *bool) {
 
 }

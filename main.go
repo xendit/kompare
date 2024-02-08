8437b542597e77bc2c3169aed34a4ab48bda3d95
Abel <abel.aguzmans@gmail.com>
2024-02-08 11:11:38 +0800

    Refactor main.go file, add some initial unit testing, add comments in some of the code, etc (#10)
    * typo
    
    * typo
    
    * basic refactoring of the main function.
    
    * refactored main and readme with intro
    
    * Add license apache 2.0 to start with,
    
    * some very basic tests for the main file
    
    * refactor-main.goFile; adding some initial unit testing
    
    * go mod tidy

--- main.go
+++ main.go
@@ -10,136 +10,65 @@ import (
 	"path/filepath"
 	"strings"
 
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 func main() {
-	// Getting and pasinf CLI argumets.
-	TheArgs := cli.PaserReader()
-	if TheArgs.Err != nil {
-		fmt.Printf("Error parsing arguments: %v\n", TheArgs.Err)
+	// Parse CLI arguments
+	args := cli.PaserReader()
+	if args.Err != nil {
+		fmt.Printf("Error parsing arguments: %v\n", args.Err)
 		return
 	}
-	clientsetToSource, err := connect.ConnectToSource(TheArgs.SourceClusterContext, &TheArgs.KubeconfigFile)
+
+	// Connect to source cluster
+	clientsetToSource, err := connect.ConnectToSource(args.SourceClusterContext, &args.KubeconfigFile)
 	if err != nil {
-		fmt.Printf("Error connecting: %v\n", err)
+		fmt.Printf("Error connecting to source cluster: %v\n", err)
 		return
 	}
-	clientsetToTarget, err := connect.ContextSwitch(TheArgs.TargetClusterContext, &TheArgs.KubeconfigFile)
+
+	// Connect to target cluster
+	clientsetToTarget, err := connect.ContextSwitch(args.TargetClusterContext, &args.KubeconfigFile)
 	if err != nil {
 		fmt.Printf("Error switching context: %v\n", err)
 		return
 	}
-	// End getting CLI arguments.
-	// Global resources.
+
+	// Determine namespace argument type
 	var sourceNameSpacesList *v1.NamespaceList
 	var sourceNameSpace *v1.Namespace
-	NameSpaceArgContent := DetectNamespacePattern(TheArgs.NamespaceName)
-	if NameSpaceArgContent == "specific" {
-		fmt.Println("Using ", TheArgs.NamespaceName, " namespace")
-		sourceNameSpace = &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: TheArgs.NamespaceName}}
+	namespaceArgType := DetectNamespacePattern(args.NamespaceName)
+	switch namespaceArgType {
+	case "specific":
+		fmt.Println("Using", args.NamespaceName, "namespace")
+		sourceNameSpace = &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: args.NamespaceName}}
 		sourceNameSpacesList = &v1.NamespaceList{Items: []v1.Namespace{*sourceNameSpace}}
-	} else if NameSpaceArgContent == "wildcard" {
-		sourceNameSpacesList, err = query.ListNameSpaces(clientsetToSource)
-		sourceNameSpacesList = filterNamespaces(sourceNameSpacesList, TheArgs.NamespaceName)
-	} else if NameSpaceArgContent == "empty" {
-		iterateGoglabObjects(clientsetToSource, clientsetToTarget, TheArgs)
+	case "wildcard":
 		sourceNameSpacesList, err = query.ListNameSpaces(clientsetToSource)
 		if err != nil {
-			fmt.Printf("Error getting namespace list: %v\n", err)
+			fmt.Printf("Error listing namespaces: %v\n", err)
+			return
+		}
+		sourceNameSpacesList = filterNamespaces(sourceNameSpacesList, args.NamespaceName)
+	case "empty":
+		iterateGoglabObjects(clientsetToSource, clientsetToTarget, args)
+		sourceNameSpacesList, err = query.ListNameSpaces(clientsetToSource)
+		if err != nil {
+			fmt.Printf("Error listing namespaces: %v\n", err)
 			return
 		}
 		sourceNameSpace = nil
 	}
-	//Iterate each namespace.
-	iterateNamespaces(sourceNameSpacesList, clientsetToSource, clientsetToTarget, TheArgs)
+
+	// Iterate over namespaces
+	iterateNamespaces(sourceNameSpacesList, clientsetToSource, clientsetToTarget, args)
+
 	fmt.Println("Finished all comparison works!")
-}
-
-func iterateNamespaces(sourceNameSpacesList *v1.NamespaceList, clientsetToSource, clientsetToTarget *kubernetes.Clientset, TheArgs cli.ArgumentsReceivedValidated) {
-	// Check if include or exclude lists are provided, or if no specific lists are provided
-	if TheArgs.Include == nil && TheArgs.Exclude == nil {
-		// If no include or exclude lists are provided, compare all resources for each namespace
-		for _, ns := range sourceNameSpacesList.Items {
-			compareAllResourcesInNamespace(clientsetToSource, clientsetToTarget, ns.Name, TheArgs)
-		}
-	} else {
-		// Compare resources based on include or exclude lists
-		for _, ns := range sourceNameSpacesList.Items {
-			compareResourcesByLists(clientsetToSource, clientsetToTarget, ns.Name, TheArgs)
-		}
-	}
-}
-
-func compareAllResourcesInNamespace(clientsetToSource, clientsetToTarget *kubernetes.Clientset, namespace string, TheArgs cli.ArgumentsReceivedValidated) {
-	fmt.Printf("Looping on Namespace: %s\n", namespace)
-
-	// Compare all resources for the namespace
-	resources := []string{"deployment", "ingress", "service", "sa", "configmap", "secret", "role", "rolebinding", "hpa", "cronjob"}
-	for _, resource := range resources {
-		fmt.Printf("%s\n", strings.Title(resource))
-		compareResource(clientsetToSource, clientsetToTarget, namespace, resource, TheArgs)
-		fmt.Printf("Finished %s for namespace: %s\n", strings.Title(resource), namespace)
-	}
-
-	fmt.Printf("... Done with all resources in ns: %s.\n", namespace)
-}
-
-func compareResourcesByLists(clientsetToSource, clientsetToTarget *kubernetes.Clientset, namespace string, TheArgs cli.ArgumentsReceivedValidated) {
-	fmt.Printf("Looping on NS: %s\n", namespace)
-
-	includeResources := TheArgs.Include
-	excludeResources := TheArgs.Exclude
-
-	// Compare resources based on include list
-	if includeResources != nil {
-		for _, resource := range includeResources {
-			fmt.Printf("%s\n", strings.Title(resource))
-			compareResource(clientsetToSource, clientsetToTarget, namespace, resource, TheArgs)
-			fmt.Printf("Finished %s for namespace: %s\n", strings.Title(resource), namespace)
-		}
-	}
-
-	// Compare resources based on exclude list
-	if excludeResources != nil {
-		allResources := []string{"deployment", "ingress", "service", "sa", "configmap", "secret", "role", "rolebinding"}
-		for _, resource := range allResources {
-			if !tools.IsInList(resource, excludeResources) {
-				fmt.Printf("%s\n", strings.Title(resource))
-				compareResource(clientsetToSource, clientsetToTarget, namespace, resource, TheArgs)
-				fmt.Printf("Finished %s for namespace: %s\n", strings.Title(resource), namespace)
-			}
-		}
-	}
-
-	fmt.Printf("... Done with all resources in ns: %s.\n", namespace)
-}
-
-func compareResource(clientsetToSource, clientsetToTarget *kubernetes.Clientset, namespace, resource string, TheArgs cli.ArgumentsReceivedValidated) {
-	switch resource {
-	case "deployment":
-		compare.CompareDeployments(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	case "ingress":
-		compare.CompareIngresses(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	case "service":
-		compare.CompareServices(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	case "sa":
-		compare.CompareServiceAccounts(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	case "configmap":
-		compare.CompareConfigMaps(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	case "secret":
-		compare.CompareSecrets(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	case "role":
-		compare.CompareRoles(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	case "rolebinding":
-		compare.CompareRoleBindings(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	case "hpa":
-		compare.CompareHPAs(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	case "cronjob":
-		compare.CompareCronJobs(clientsetToSource, clientsetToTarget, namespace, TheArgs)
-	}
 }
 
 func iterateGoglabObjects(clientsetToSource, clientsetToTarget *kubernetes.Clientset, args cli.ArgumentsReceivedValidated) {
@@ -201,9 +130,126 @@ func iterateGoglabObjects(clientsetToSource, clientsetToTarget *kubernetes.Clien
 	}
 }
 
+func iterateNamespaces(sourceNameSpacesList *v1.NamespaceList, clientsetToSource, clientsetToTarget *kubernetes.Clientset, TheArgs cli.ArgumentsReceivedValidated) {
+	// Check if include or exclude lists are provided, or if no specific lists are provided
+	if TheArgs.Include == nil && TheArgs.Exclude == nil {
+		// If no include or exclude lists are provided, compare all resources for each namespace
+		for _, ns := range sourceNameSpacesList.Items {
+			compareAllResourcesInNamespace(clientsetToSource, clientsetToTarget, ns.Name, TheArgs)
+		}
+	} else {
+		// Compare resources based on include or exclude lists
+		for _, ns := range sourceNameSpacesList.Items {
+			compareResourcesByLists(clientsetToSource, clientsetToTarget, ns.Name, TheArgs)
+		}
+	}
+}
+
+func compareAllResourcesInNamespace(clientsetToSource, clientsetToTarget *kubernetes.Clientset, namespace string, TheArgs cli.ArgumentsReceivedValidated) {
+	fmt.Printf("Looping on NS: %s\n", namespace)
+
+	// Compare all resources for the namespace
+	resources := []string{"deployment", "ingress", "service", "sa", "configmap", "secret", "role", "rolebinding", "hpa", "cronjob"}
+	for _, resource := range resources {
+		fmt.Printf("%s\n", strings.Title(resource))
+		compareResource(clientsetToSource, clientsetToTarget, namespace, resource, TheArgs)
+		fmt.Printf("Finished %s for namespace: %s\n", strings.Title(resource), namespace)
+	}
+
+	fmt.Printf("... Done with all resources in ns: %s.\n", namespace)
+}
+
+func compareResourcesByLists(clientsetToSource, clientsetToTarget *kubernetes.Clientset, namespace string, TheArgs cli.ArgumentsReceivedValidated) {
+	fmt.Printf("Looping on NS: %s\n", namespace)
+
+	includeResources := TheArgs.Include
+	excludeResources := TheArgs.Exclude
+
+	// Create a title case converter for English
+	titleCase := cases.Title(language.English)
+
+	// Define all resources
+	allResources := []string{"deployment", "ingress", "service", "sa", "configmap", "secret", "role", "rolebinding"}
+
+	// Compare resources based on include list
+	if includeResources != nil {
+		for _, resource := range includeResources {
+			titleResource := titleCase.String(resource)
+			fmt.Printf("%s\n", titleResource)
+			compareResource(clientsetToSource, clientsetToTarget, namespace, resource, TheArgs)
+			fmt.Printf("Finished %s for namespace: %s\n", titleResource, namespace)
+		}
+	}
+
+	// Compare resources based on exclude list
+	if excludeResources != nil {
+		for _, resource := range allResources {
+			// Check if resource is not in the exclude list
+			if !tools.IsInList(resource, excludeResources) {
+				titleResource := titleCase.String(resource)
+				fmt.Printf("%s\n", titleResource)
+				compareResource(clientsetToSource, clientsetToTarget, namespace, resource, TheArgs)
+				fmt.Printf("Finished %s for namespace: %s\n", titleResource, namespace)
+			}
+		}
+	}
+
+	fmt.Printf("... Done with all resources in ns: %s.\n", namespace)
+}
+
+func iterateGoglabObjects(clientsetToSource, clientsetToTarget *kubernetes.Clientset, TheArgs cli.ArgumentsReceivedValidated) {
+	// Comparing namespaces.
+	// notice that this fuction returns a diff to be used if we use tests instead of CLI
+	doSomething := false
+	if TheArgs.Include != nil &&
+		tools.AreAnyInLists([]string{"namespace", "crd", "clusterrole", "clusterrolebinding"}, TheArgs.Include) {
+		if tools.IsInList("namespace", TheArgs.Include) == true {
+			compare.CompareNameSpaces(clientsetToSource, clientsetToTarget, TheArgs)
+		}
+		if tools.IsInList("crd", TheArgs.Include) == true {
+			compare.CompareCRDs(TheArgs.TargetClusterContext, TheArgs.KubeconfigFile, TheArgs)
+		}
+		if tools.IsInList("clusterrole", TheArgs.Include) == true {
+			compare.CompareClusterRoles(clientsetToSource, clientsetToTarget, TheArgs)
+		}
+		if tools.IsInList("clusterrolebinding", TheArgs.Include) == true {
+			compare.CompareClusterRoleBindings(clientsetToSource, clientsetToTarget, TheArgs)
+		}
+		doSomething = true
+	}
+	if TheArgs.Exclude != nil &&
+		tools.AreAnyInLists([]string{"namespace", "crd", "clusterrole", "clusterrolebinding"}, TheArgs.Exclude) {
+		if tools.IsInList("namespace", TheArgs.Exclude) == false {
+			compare.CompareNameSpaces(clientsetToSource, clientsetToTarget, TheArgs)
+		}
+		if tools.IsInList("crd", TheArgs.Exclude) == false {
+			compare.CompareCRDs(TheArgs.TargetClusterContext, TheArgs.KubeconfigFile, TheArgs)
+		}
+		if tools.IsInList("clusterrole", TheArgs.Exclude) == false {
+			compare.CompareClusterRoles(clientsetToSource, clientsetToTarget, TheArgs)
+		}
+		if tools.IsInList("clusterrolebinding", TheArgs.Exclude) == false {
+			compare.CompareClusterRoleBindings(clientsetToSource, clientsetToTarget, TheArgs)
+		}
+		doSomething = true
+	}
+	if TheArgs.Include == nil && TheArgs.Exclude == nil {
+		compare.CompareNameSpaces(clientsetToSource, clientsetToTarget, TheArgs)
+		compare.CompareCRDs(TheArgs.TargetClusterContext, TheArgs.KubeconfigFile, TheArgs)
+		compare.CompareClusterRoles(clientsetToSource, clientsetToTarget, TheArgs)
+		compare.CompareClusterRoleBindings(clientsetToSource, clientsetToTarget, TheArgs)
+		doSomething = true
+	}
+	if doSomething {
+		fmt.Println("Done comparing Kuberentes global objects.")
+	}
+}
+
 // filterNamespaces filters namespaces based on the wildcard pattern
 func filterNamespaces(namespaces *v1.NamespaceList, pattern string) *v1.NamespaceList {
-	var matchingNamespaces v1.NamespaceList
+	matchingNamespaces := v1.NamespaceList{
+		Items: []v1.Namespace{},
+	}
 	for _, ns := range namespaces.Items {
 		if matchWildcard(ns.Name, pattern) {
 			matchingNamespaces.Items = append(matchingNamespaces.Items, ns)
--- main_test.go
+++ main_test.go
@@ -2,26 +2,115 @@ package main
 
 import (
 	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestParseCLIArguments(t *testing.T) {
-	// Your test cases for parsing CLI arguments
+func TestFilterNamespaces(t *testing.T) {
+	// Create a sample namespace list
+	namespaces := &corev1.NamespaceList{
+		Items: []corev1.Namespace{
+			{ObjectMeta: metav1.ObjectMeta{Name: "test"}},
+			{ObjectMeta: metav1.ObjectMeta{Name: "example"}},
+			{ObjectMeta: metav1.ObjectMeta{Name: "hello"}},
+			{ObjectMeta: metav1.ObjectMeta{Name: "world"}},
+		},
+	}
+
+	// Test case when there are matching namespaces
+	result := filterNamespaces(namespaces, "*l*")
+	expected := &corev1.NamespaceList{
+		Items: []corev1.Namespace{
+			{ObjectMeta: metav1.ObjectMeta{Name: "hello"}},
+			{ObjectMeta: metav1.ObjectMeta{Name: "world"}},
+			{ObjectMeta: metav1.ObjectMeta{Name: "example"}},
+		},
+	}
+	if !equalNamespaceLists(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+
+	// Test case when there are no matching namespaces
+	result = filterNamespaces(namespaces, "abc")
+	expected = &corev1.NamespaceList{}
+	if !equalNamespaceLists(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+
+	// Test case when the input namespace list is empty
+	emptyNamespaces := &corev1.NamespaceList{}
+	result = filterNamespaces(emptyNamespaces, "*")
+	expected = &corev1.NamespaceList{}
+	if !equalNamespaceLists(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
 }
 
-func TestEstablishConnections(t *testing.T) {
-	// Your test cases for establishing connections to clusters
+// Function to compare NamespaceLists
+func equalNamespaceLists(a, b *corev1.NamespaceList) bool {
+	if len(a.Items) != len(b.Items) {
+		return false
+	}
+
+	namespacesA := make(map[string]int)
+	namespacesB := make(map[string]int)
+
+	for _, ns := range a.Items {
+		namespacesA[ns.Name]++
+	}
+
+	for _, ns := range b.Items {
+		namespacesB[ns.Name]++
+	}
+
+	for name, countA := range namespacesA {
+		countB := namespacesB[name]
+		if countA != countB {
+			return false
+		}
+	}
+
+	return true
 }
 
-func TestIterateNamespaces(t *testing.T) {
-	// Your test cases for iterating over namespaces
+func TestMatchWildcard(t *testing.T) {
+	// Test case when the string matches the wildcard pattern
+	result := matchWildcard("hello", "h*llo")
+	if !result {
+		t.Error("Expected true, but got false")
+	}
+
+	// Test case when the string does not match the wildcard pattern
+	result = matchWildcard("world", "h*llo")
+	if result {
+		t.Error("Expected false, but got true")
+	}
+
+	// Test case when there is an error in pattern matching
+	result = matchWildcard("hello", "[")
+	if result {
+		t.Error("Expected false, but got true")
+	}
 }
 
-func TestCompareDeployments(t *testing.T) {
-	// Your test cases for comparing deployments
-}
+func TestDetectNamespacePattern(t *testing.T) {
+	// Test case for empty pattern
+	result := DetectNamespacePattern("")
+	if result != "empty" {
+		t.Errorf("Expected 'empty' but got '%s'", result)
+	}
 
-func TestCompareServices(t *testing.T) {
-	// Your test cases for comparing services
-}
+	// Test case for wildcard pattern
+	result = DetectNamespacePattern("*.example.com")
+	if result != "wildcard" {
+		t.Errorf("Expected 'wildcard' but got '%s'", result)
+	}
 
-// Add similar tests for other resource comparison functions
+	// Test case for specific pattern
+	result = DetectNamespacePattern("example.com")
+	if result != "specific" {
+		t.Errorf("Expected 'specific' but got '%s'", result)
+	}
+}

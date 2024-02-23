1e9cb149921be9aa92feefe8ff7c9020b250f271
Abel Guzman Sanchez <abel.guzman@xendit.co>
2024-02-23 17:08:11 +0800

    save-output-to-file-01; Commented out candidate way to implement that. it's failing

--- main.go
+++ main.go
@@ -23,6 +23,36 @@ func main() {
 		err := fmt.Errorf("error parsing arguments: %v", args.Err)
 		panic(err)
 	}
+	// var file *os.File
+	// if args.FileOutput != "" {
+	// 	_, fileString, _ := tools.IsValidPath(args.FileOutput)
+	// 	var err error
+	// 	file, err = os.Create(fileString)
+	// 	if err != nil {
+	// 		fmt.Println("Error opening file:", err)
+	// 		os.Exit(1)
+	// 	}
+	// 	defer file.Close()
+	// }
+	// // Create a multi-writer to write to both stdout and the file (if specified)
+	// var writers []io.Writer
+	// writers = append(writers, os.Stdout)
+	// if file != nil {
+	// 	writers = append(writers, file)
+	// }
+	// multiWriter := io.MultiWriter(writers...)
+
+	// // Use a goroutine to copy output to both stdout and the file
+	// go func(out io.Writer) {
+	// 	for {
+	// 		// Copy output to both stdout and the file
+	// 		_, err := io.Copy(out, os.Stdin)
+	// 		if err != nil {
+	// 			fmt.Println("Error copying output:", err)
+	// 			return
+	// 		}
+	// 	}
+	// }(multiWriter)
 
 	// Connect to source cluster
 	clientsetToSource, err := connect.ConnectToSource(args.SourceClusterContext, &args.KubeconfigFile)
